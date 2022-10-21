c73e2902f0047861b46faf136bef396740db5525
Dragos A. Bobu <46262576+BobuDragos@users.noreply.github.com>
2022-10-21 13:19:47 +0300

    Update problem1.py

--- Lab4/problem1.py
+++ Lab4/problem1.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 import pandas as pd
 import pymc3 as pm
 import arviz as az
 
 model = pm.Model()
 
+alpha = 6
+
 with model:
     clienti = pm.Poisson('C', 20)
 
-    statieGatit = pm.exp('SG', 6)
-    
+    statieGatit = np.exp('SG', alpha)
