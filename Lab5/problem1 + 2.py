11df560c2440de837c208906b1fcf1de79b842e0
BobuDragos <46262576+BobuDragos@users.noreply.github.com>
2022-11-02 22:34:19 +0200

    lab5 - homework

--- Lab5/problem1 + 2.py
+++ Lab5/problem1 + 2.py
@@ -54,3 +54,23 @@ game_model.local_independencies("Player1")
 
 game_model.get_independencies()
 
+
+model_inferential = VariableElimination(game_model)
+
+#EX 2 
+# a - esti jucatorul 1 si ai primit un rege de frunza, ar trebui sa pariezi sau nu?
+
+query1 = model_inferential.query(variables=['P1'], evidence={'Card_first_player': 1})
+print('first query 1:' ,query1)
+
+#b - esti jucatorul 2 si ai primit un rege de inima iar jucatorul 1 a decis sa parieze , cum ar trb sa decizi?
+query2 = model_inferential.query(variables=['P2'], evidence={'P1': 0, 'Card_second_player': 2})
+print('first query 2:',query2)
+
+# c - schimbati parametrii strategiilor de pariere - schimb P1 si P2
+
+P1_modified = TabularCPD(variable = 'P1', variable_card = 2,values = [[1, 0.2, 0.6, 0.3, 0],[0, 0.8, 0.4, 0.7, 1]], evidence = ['Card_first_player'],evidence_card=[5])
+
+P2_modified = TabularCPD(variable = 'P2', variable_card=3, values=[[1, 0.8, 0.4, 0.3, 0, 1, 0.8, 0.4, 0.2, 0],[0, 0.2, 0.6, 0.7, 1, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0.2, 0.6, 0.8, 1]], evidence=['P1', 'Card_second_player'], evidence_card=[2, 5])
+
+print(P1_modified, P2_modified)
